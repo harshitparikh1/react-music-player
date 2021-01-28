5e6e584ffb1d6cdec0c6439f806e7e114aa0dad1
Harshit Parikh <harshitparikh12@gmail.com>
2021-01-28 11:49:35 +0530

    Added 2 new songs and solved error related to hooks.

--- src/App.js
+++ src/App.js
@@ -10,22 +10,22 @@ function App() {
       src: "./music/on-n-on.mp3"
     },
     {
-      title: "Song 2",
-      artist: "Artist 2",
+      title: "On & On",
+      artist: "Cartoon",
       img_src: "./images/song-2.jpg",
       src: "./music/somebody-new.mp3"
     },
     {
-      title: "Song 3",
-      artist: "Artist 3",
+      title: "The Nights",
+      artist: "Avicii",
       img_src: "./images/song-3.jpg",
-      src: "./music/on-n-on.mp3"
+      src: "./music/Avicii-The-Nights.mp3"
     },
     {
-      title: "Song 4",
-      artist: "Artist 4",
+      title: "On my way",
+      artist: "Alan Walker",
       img_src: "./images/song-4.jpg",
-      src: "./music/somebody-new.mp3"
+      src: "./music/On-My-Way.mp3"
     }
   ]);
 
@@ -41,6 +41,17 @@ function App() {
       }
     });
   }, [currentSongIndex]);
+
+  // To remove the console error we can use the below code but this causes infinite loop.
+  // useEffect(
+  //   setNextSongIndex(() => {
+  //     if (currentSongIndex + 1 > songs.length - 1) {
+  //       return 0;
+  //     } else {
+  //       return currentSongIndex + 1;
+  //     }
+  //   })
+  // , [currentSongIndex]);
 
   return (
     <div className="App"> 
