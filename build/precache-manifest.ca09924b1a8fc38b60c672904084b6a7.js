63699be1c2d14eb0ec6ae7f92b10a3473b1b6852
Harshit Parikh <harshitparikh12@gmail.com>
2021-01-28 22:31:51 +0530

    Added build files

---
+++ build/precache-manifest.ca09924b1a8fc38b60c672904084b6a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee838c2480268f2b0d2875b0d74b887",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e97c5cf0bb0fae35fd4",
+    "url": "/static/css/main.c74fb819.chunk.css"
+  },
+  {
+    "revision": "a57c7675315d45c939a2",
+    "url": "/static/js/2.9129c9b3.chunk.js"
+  },
+  {
+    "revision": "f75561adb7f4db3b610232e7231a7ed4",
+    "url": "/static/js/2.9129c9b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e97c5cf0bb0fae35fd4",
+    "url": "/static/js/main.d05a1f33.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
